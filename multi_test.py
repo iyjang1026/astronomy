00ba1b734833bc039ff8525dc6fd98c5dc550de0
장인영 <jang-in-yeong@jang-in-yeong-ui-MacBookAir.local>
2025-06-19 22:42:02 +0900

    master flat complete

--- multi_test.py
+++ multi_test.py
@@ -32,40 +32,37 @@ def se_mask(file):
 warnings.filterwarnings('ignore')
 def mask(path):
     file = glob.glob(path + '/N*.fits')
-    data_list = []
     scale_list = []
+    mode0 = []
     bar0 = progressbar.ProgressBar(maxval=len(file), widgets=['[',progressbar.Timer(),']',progressbar.Bar()]).start()
     for n in range(len(file)):
         data = se_mask(file[n])
         mode1 = mode(data[~np.isnan(data)])[0]
-        scaled_data = (data - mode1)/np.std(data[~np.isnan(data)])
-        data_list.append(data)
+        scaled_data = (data - mode1)/mode1
         scale_list.append(scaled_data)
+        mode0.append(mode1)
         bar0.update(n)
     bar0.finish()
-    return np.array(data_list), np.array(scale_list)
+    mode_tot = np.median(np.array(mode0))
+    return np.array(scale_list), mode_tot
 
-def index(arr, arr1,q):
+def index(arr,q):
     l,x,y = arr.shape
     data_zero = np.zeros((x,y))
     for i in range(x):
         for j in range(y):
-            arr_data = arr1[:,i,j]
-            arr_t = arr[:,i,j]
+            arr_data = arr[:,i,j]
             weak_data = weakref.ref(arr_data)()
-            median = np.median(weak_data[~np.isnan(weak_data)])
-            idx_median = np.abs(weak_data - median).argmin()
-            flat_data = arr_t[idx_median]
-            data_zero[i,j] += flat_data
+            data_zero[i,j] += np.median(weak_data[~np.isnan(weak_data)])
     q.put(weakref.ref(data_zero)())
     
 def main():
     multiprocessing.freeze_support()
-    data_tot, scale_tot = mask('/volumes/ssd/2025-06-16/LIGHT/DB_subed/r')
+    data_tot, mode_tot = mask('/volumes/ssd/2025-06-16/LIGHT/DB_subed/r')
     tasks=[]
     q = JoinableQueue()
     for k in range(multiprocessing.cpu_count()):
-        thread = Process(target=index, args=(data_tot,scale_tot, q))
+        thread = Process(target=index, args=(data_tot, q))
         tasks.append(thread)
         thread.start()
     
@@ -73,7 +70,8 @@ def main():
     for task in tasks:
         task.kill()
         task.join()
-    fits.writeto('/volumes/ssd/2025-06-16/test_dark_sky.fits', np.array(result), overwrite=True)
+    data_final = np.array(result)*mode_tot + mode_tot
+    fits.writeto('/volumes/ssd/2025-06-16/test_dark_sky.fits', data_final , overwrite=True)
     print(f'processes finished')
     
 
